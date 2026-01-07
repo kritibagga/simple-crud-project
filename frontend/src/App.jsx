99f9dc5d1b18ccf779db90617a5557a9b2f97729
kritibagga <bagga.kriti@gmail.com>
2026-01-07 15:03:06 -0500

    added sidebar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,8 +13,6 @@ function App() {
 		role: "",
 	});
 
-
-
 	const handleChange = (e) => {
 		const { name, value } = e.target;
 		setFormData((prev) => ({
@@ -43,164 +41,231 @@ function App() {
 		}
 	};
 
-  useEffect(() => {
+	useEffect(() => {
 		// eslint-disable-next-line react-hooks/set-state-in-effect
 		fetchUsers();
 	}, []);
 
-	const handleEdit=(user)=>{
+	const handleEdit = (user) => {
 		setEditingId(user.id);
 		setFormData({
-			name:user.name,
-			email:user.email,
-			role:user.role,
-		})
+			name: user.name,
+			email: user.email,
+			role: user.role,
+		});
+	};
+	const handleDelete = async (id) => {
+		if (!window.confirm("Are you sure you want to delete this record")) return;
 
-	}
-	const handleDelete=async(id)=>{
-		if(!window.confirm("Are you sure you want to delete this record")) return;
-
-		try{
+		try {
 			await axios.delete(`${API_BASE_URL}/${id}`);
 			await fetchUsers();
-		}catch (err){
-			console.error("Error deleting user", err)
+		} catch (err) {
+			console.error("Error deleting user", err);
 		}
+	};
 
-
-
-	}
-
-	const handleSubmit=async(e)=>{
+	const handleSubmit = async (e) => {
 		e.preventDefault();
 
-		try{
-			if(isEditingId===null){
-				await axios.post(API_BASE_URL, formData)
-			}else{
-				await axios.put(`${API_BASE_URL}/${isEditingId}`, formData)
+		try {
+			if (isEditingId === null) {
+				await axios.post(API_BASE_URL, formData);
+			} else {
+				await axios.put(`${API_BASE_URL}/${isEditingId}`, formData);
 			}
 
 			await fetchUsers();
 			resetForm();
-		}catch(err){
-			console.error("Error saving user", err)
+		} catch (err) {
+			console.error("Error saving user", err);
 		}
-	}
+	};
 
 	return (
-		<div className='container my-5'>
-			<h1 className='mb-4 text-center'> Simple User Management</h1>
-			<div className='card mb-4'>
-				<div className='card-header'>
-					{isEditingId ? "Edit User" : "Add new user"}
+		<>
+			<nav className='navbar navbar-expand-lg navbar-light bg-light'>
+				<div className='container ml-0'>
+
+					<a
+						className='navbar-brand'
+						href='#'>
+						User Management App
+					</a>
+
+					<button
+						className='navbar-toggler'
+						type='button'
+						data-bs-toggle='collapse'
+						data-bs-target='#navbarSupportedContent'
+						aria-controls='navbarSupportedContent'
+						aria-expanded='false'
+						aria-label='Toggle navigation'>
+						<span className='navbar-toggler-icon'></span>
+					</button>
+
+					<div
+						className='collapse navbar-collapse'
+						id='navbarSupportedContent'>
+						<ul className='navbar-nav me-auto mb-2 mb-lg-0'>
+							<li className='nav-item'>
+								<a
+									className='nav-link active'
+									aria-current='page'
+									href='#'>
+									Home
+								</a>
+							</li>
+							<li className='nav-item'>
+								<a
+									className='nav-link'
+									href='#'>
+									Link
+								</a>
+							</li>
+						</ul>
+
+
+					</div>
 				</div>
-				<div className='card-body'>
-					<form  onSubmit={handleSubmit} className='row g-3'>
-						<div className='col-md-4'>
-							<label className='form-label'>Name </label>
-							<input
-								name='name'
-								value={formData.name}
-								onChange={handleChange}
-								className='form-control'
-								required
-							/>
-						</div>
+			</nav>
+			<div class="container-fluid">
+        <div class="row">
+            <nav id="sidebar" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
+			<ul className="nav nav-pills flex-column mb-auto">
+				<li className="nav-item">
+					<a className="nav-link link-dark" href="#"><i className="fa-solid fa-home me-2"></i>Home</a>
+				</li>
+				<li className="nav-item">
+					<a className="nav-link link-dark" href="#"><i className="fa-solid fa-store me-2"></i>Store</a>
+				</li>
+				<li className="nav-item">
+					<a className="nav-link link-dark" href="#"><i className="fa-solid fa-chart-pie me-2"></i>Charts</a>
+				</li>
+				<li className="nav-item">
+					<a className="nav-link link-dark" href="#"><i className="fa-solid fa-envelope me-2"></i>Contact</a>
+				</li>
+			</ul>
 
-						<div className='col-md-4'>
-							<label className='form-label'>Email </label>
-							<input
-								name='email'
-								type='email'
-								value={formData.email}
-								onChange={handleChange}
-								className='form-control'
-								required
-							/>
-						</div>
+            </nav>
+            <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
 
-						<div className='col-md-4'>
-							<label className='form-label'>Role </label>
-							<input
-								name='role'
-								value={formData.role}
-								onChange={handleChange}
-								className='form-control'
-								required
-							/>
-						</div>
+			<div className='container my-5'>
+				<h1 className='mb-4 text-center'> Simple User Management</h1>
+				<div className='card mb-4'>
+					<div className='card-header'>
+						{isEditingId ? "Edit User" : "Add new user"}
+					</div>
+					<div className='card-body'>
+						<form
+							onSubmit={handleSubmit}
+							className='row g-3'>
+							<div className='col-md-4'>
+								<label className='form-label'>Name </label>
+								<input
+									name='name'
+									value={formData.name}
+									onChange={handleChange}
+									className='form-control'
+									required
+								/>
+							</div>
 
-						<div className='col-12 d-flex gap-2'>
-							<button
-								type='submit'
-								className='btn btn-primary'>
-								{isEditingId ? "Update" : "Add"}
-							</button>
+							<div className='col-md-4'>
+								<label className='form-label'>Email </label>
+								<input
+									name='email'
+									type='email'
+									value={formData.email}
+									onChange={handleChange}
+									className='form-control'
+									required
+								/>
+							</div>
 
-							{isEditingId && (
+							<div className='col-md-4'>
+								<label className='form-label'>Role </label>
+								<input
+									name='role'
+									value={formData.role}
+									onChange={handleChange}
+									className='form-control'
+									required
+								/>
+							</div>
+
+							<div className='col-12 d-flex gap-2'>
 								<button
-									type='button'
-									className='btn btn-secondary'
-									onClick={resetForm}>
-									Cancel
+									type='submit'
+									className='btn btn-primary'>
+									{isEditingId ? "Update" : "Add"}
 								</button>
-							)}
-						</div>
-					</form>
+
+								{isEditingId && (
+									<button
+										type='button'
+										className='btn btn-secondary'
+										onClick={resetForm}>
+										Cancel
+									</button>
+								)}
+							</div>
+						</form>
+					</div>
+				</div>
+
+				<div className='card'>
+					<div className='card-header'>Users List</div>
+					<div className='card-body table-body'>
+						{users.length === 0 ? (
+							<p>No Users yet. Add one above.</p>
+						) : (
+							<div className='table-responsive'>
+								<table className='table table-striped table-bordered table-hover'>
+									<thead className='table-light'>
+										<tr>
+											<th>Id</th>
+											<th>Name</th>
+											<th>Email</th>
+											<th>Role</th>
+											<th style={{ width: "150px" }}>Actions</th>
+										</tr>
+									</thead>
+									<tbody>
+										{users.map((u) => (
+											<tr key={u.id}>
+												<td>{u.id}</td>
+												<td>{u.name}</td>
+												<td>{u.email}</td>
+												<td>{u.role}</td>
+												<td>
+													<button
+														className='btn btn-sm btn-outline-primary me-2'
+														onClick={() => handleEdit(u)}>
+														Edit
+													</button>
+
+													<button
+														className='btn btn-sm btn-outline-danger me-2'
+														onClick={() => handleDelete(u.id)}>
+														Delete
+													</button>
+												</td>
+											</tr>
+										))}
+									</tbody>
+								</table>
+							</div>
+						)}
+					</div>
 				</div>
 			</div>
 
-      <div className="card">
-        <div className="card-header">
-          Users List
+            </main>
         </div>
-		<div className="card-body table-body">
-			{users.length===0?(<p>No Users yet. Add one above.</p>):(
-				<div className="table-responsive">
-					<table className="table table-striped table-bordered table-hover">
-						<thead className="table-light">
-							<tr>
-								<th>Id</th>
-								<th>Name</th>
-								<th>Email</th>
-								<th>Role</th>
-								<th style={{width:"150px"}}>Actions</th>
+    </div>
 
-							</tr>
-
-						</thead>
-						<tbody>
-							{
-								users.map((u)=>(
-									<tr key={u.id}>
-										<td>{u.id}</td>
-										<td>{u.name}</td>
-										<td>{u.email}</td>
-										<td>{u.role}</td>
-										<td>
-											<button className="btn btn-sm btn-outline-primary me-2" onClick={()=>handleEdit(u)}>Edit</button>
-
-											<button className="btn btn-sm btn-outline-danger me-2" onClick={()=>handleDelete(u.id)}>Delete</button>
-										</td>
-
-									</tr>
-
-								))
-
-
-							}
-						</tbody>
-
-					</table>
-
-				</div>
-
-			)}
-		</div>
-
-      </div>
-		</div>
+		</>
 	);
 }
 
